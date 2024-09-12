d91db2f300cde6c2a859effcb3d9dbd336f35950
Colin Perry <123716435+colinperry07@users.noreply.github.com>
2024-09-11 22:33:33 -0400

    Update Main.java
    the same as before except now you can continue after the first equation and use the previous result as the first number in the next equation...

--- Main.java
+++ Main.java
@@ -7,49 +7,92 @@ public class Main {
 		// Creates a new object using the java.util.Scanner package
 		// (System.in) references the InputStreamReader class for the Scanner class object
 		Scanner input = new Scanner(System.in); 
+		boolean calcOn = true;
+		boolean calcLooped = false;
+		Double result = null;
 		
-		// Defines the variables
-	    char operator;
-	    Double number1, number2, result;
-		
-	    // Takes first number as input
-	    System.out.println("Enter first number:");
-	    number1 = input.nextDouble();
-	    
-	    // Takes operator as input
-	    System.out.println("Choose an operator: +, -, *, or /");
-	    operator = input.next().charAt(0);
-	    
-	    // Takes second number as input
-	    System.out.println("Enter second number:");
-	    number2 = input.nextDouble();
-	    
-	    // Acts as a more refined alternative to multiple if/else if... statements
-	    switch (operator) {
-	    
-		    case '+':
-		    	result = number1 + number2;
-		    	System.out.println(number1 + " + " + number2 + " = " + result);
-		    	break;
-		    case '-':
-		    	result = number1 - number2;
-		    	System.out.println(number1 + " - " + number2 + " = " + result);
-		    	break;
-		    case '*':
-		    	result = number1 * number2;
-		    	System.out.println(number1 + " * " + number2 + " = " + result);
-		    	break;
-		    case '/':
-		    	result = number1 / number2;
-		    	System.out.println(number1 + " / " + number2 + " = " + result);
-		    	break;
+		while (calcOn) {
+			// Defines the variables
+			char operator;
+		    Double number1 = null, number2;
+		    String ccText;
+		    
+		    boolean resultFound = false;
+		    
+		    while (!resultFound) {
+		    	if (!calcLooped) {
+			    	System.out.println("Enter first number:");
+				    number1 = input.nextDouble();
+				    resultFound = true;
+			    } else {
+			    	System.out.println("Enter new number or ans (for previous answer):");
+			    	
+			    	String userInput = input.next(); // Takes input as a string
+			    	
+			    	switch (userInput.toLowerCase()) {
+				    	default:
+				    		try {
+				    			number1 = Double.parseDouble(userInput); // Converts the string input to a double
+				    		} catch (Exception e){ // if anything except for Ans/ANs/ANS/ans, will give error
+				    			System.out.println("Invalid Input!");
+				    			break;
+				    		}
+				    		break;
+				    	case "ans":
+				    		number1 = result;
+				    		System.out.println(number1);
+				    		resultFound = true;
+			    	}
+			    }
+		    }
+		    
+		    
+		    System.out.println("Choose an operator: +, -, *, or /");
+		    operator = input.next().charAt(0);
+		    
+		    System.out.println("Enter second number:");
+		    number2 = input.nextDouble();
+		    
+		    switch (operator) {
+		    
+			    case '+':
+			    	result = number1 + number2;
+			    	System.out.println(number1 + " + " + number2 + " = " + result);
+			    	break;
+			    case '-':
+			    	result = number1 - number2;
+			    	System.out.println(number1 + " - " + number2 + " = " + result);
+			    	break;
+			    case '*':
+			    	result = number1 * number2;
+			    	System.out.println(number1 + " * " + number2 + " = " + result);
+			    	break;
+			    case '/':
+			    	result = number1 / number2;
+			    	System.out.println(number1 + " / " + number2 + " = " + result);
+			    	break;
+			    default:
+			    	System.out.println("Invalid operator!");
+			    	break;
+		    }
+		    
+		    System.out.println("Continue? (y or n)");
+		    ccText = input.next();
+		    
+		    switch (ccText.toLowerCase()) {
 		    default:
-		    	System.out.println("Invalid operator!");
+		    	System.out.println("Answer unrecognized, shutting down...");
+		    	calcOn = false;
+		    case "y":
+		    	calcLooped = true;
 		    	break;
+		    case "n":
+		    	System.out.println("Calculator shutting down...");
+		    	calcOn = false;
+		    }
+		}
 	    
-	    }
-	    
-	    input.close(); // Tbh not sure what this does, just know that it gives me a warning if I don't
+		input.close();
 	    
 	}
 }
